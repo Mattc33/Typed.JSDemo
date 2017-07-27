43a53db69456eeadf160dc87ef1115973fad655d
Matthew Chan <mattc3303@gmail.com>
2017-07-26 22:48:53 -0400

    typed.js playground

---
+++ typedController.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 60,
+    startDelay: 500,
+    backSpeed: 30,
+    showCursor: true,
+    cursorChar: '_',
+    loop: true
+});
